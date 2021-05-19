dcaaac0ea8a20a9797391524b2c14c05502afab5
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-19 22:09:27 +1000

    console table installed

--- empTrackerSchema.sql
+++ empTrackerSchema.sql
@@ -30,3 +30,54 @@ SELECT * FROM department;
 select * from role;
 select * from employee;
 
+
+INSERT INTO department (dept_name)
+VALUES ("Administration");
+
+INSERT INTO department (dept_name)
+VALUES ("Engineering");
+
+INSERT INTO department (dept_name)
+VALUES ("Legal");
+
+INSERT INTO department (dept_name)
+VALUES ("Finance");
+
+INSERT INTO department (dept_name)
+VALUES ("Communications");
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Susan", "Smith", 12, 34 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Brian", "Travis", 13, 45);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Chops", "McGoven", 23, 67);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Beth", "Tranh", 34, 67);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Cheryl", "Ng", 45, 78);
+
+INSERT INTO role (role_title, salary, dept_id)
+VALUES ("Senior Engineer", "90000", 40);
+
+INSERT INTO role (role_title, salary, dept_id)
+VALUES ("Junior Engineer", "60000", 45);
+
+INSERT INTO role (role_title, salary, dept_id)
+VALUES ("CFO", "100000", 60);
+
+INSERT INTO role (role_title, salary, dept_id)
+VALUES ("Communications Manager", "50000", 62);
+
+INSERT INTO role (role_title, salary, dept_id)
+VALUES ("Administration", "50000", 47);
+
+
+
+
+
